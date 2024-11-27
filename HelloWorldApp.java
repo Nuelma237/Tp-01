a508bf390957fb4cd5fa401835eccc89797030bc
Nuelma237 <elomomanuel2006@gmail.com>
2024-11-28 00:41:53 +0100

    Add files via upload

---
+++ HelloWorldApp.java
@@ -0,0 +1,9 @@
+ /**
+4. * The HelloWorldApp class implements an application that
+5. * simply prints "Hello World!" to standard output.
+6. */
+7. public class HelloWorldApp {
+8. public static void main(String[] args) {
+9. System.out.println("Hello World!"); // Display the string.
+10. }
+}
